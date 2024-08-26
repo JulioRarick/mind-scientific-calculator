5d3933d2f9653a6190d11101f128745410b94169
Julio Rarick <juliorarick@gmail.com>
2024-08-26 19:25:09 -0300

    💄 update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Calculator } from '@/components/calculator/calculator'
 import { Header } from '@/components/header'
 import { CalculatorProvider } from '@/context/calculator-context'
@@ -6,10 +8,16 @@ export default function Home() {
   return (
     <main className="flex h-screen min-h-screen w-full flex-col items-center">
       <Header />
-      <div className="flex h-full w-full flex-1 items-center justify-center">
+      <div className="flex h-full w-full flex-1 flex-col items-center justify-center">
         <CalculatorProvider>
           <Calculator />
         </CalculatorProvider>
+        <Link
+          href="https://www.juliorarick.tech"
+          className="block p-8 text-sm hover:underline-offset-2 lg:hidden"
+        >
+          &copy; Code by Julio Rarick
+        </Link>
       </div>
     </main>
   )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Calculator } from 'lucide-react'
+import Link from 'next/link'
 
 import { ThemeToggle } from './theme-toggle'
 
@@ -7,13 +8,16 @@ export function Header() {
     <header className="flex h-20 w-full items-center justify-center">
       <article className="flex w-full max-w-screen-md items-center justify-between px-8">
         <h1 className="flex items-center justify-center gap-1 text-2xl font-bold tracking-tighter">
-          <Calculator className="h-9 w-9 text-emerald-500" />
+          <Calculator className="h-9 w-9 text-cyan-600" />
           mind.sc
         </h1>
         <ThemeToggle />
-        <span className="hidden text-sm lg:block">
+        <Link
+          href="https://www.juliorarick.tech"
+          className="hidden text-sm hover:underline-offset-2 lg:block"
+        >
           &copy; Code by Julio Rarick
-        </span>
+        </Link>
       </article>
     </header>
   )
